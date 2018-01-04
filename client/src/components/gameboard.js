cb4e2138bf9bb1eb5d0e3848bb286edbe98c7197
scollins33 <sean_collins@gmx.com>
2018-01-03 21:35:23 -0500

    tile loop and callbacks added

--- client/src/components/gameboard.js
+++ client/src/components/gameboard.js
@@ -4,23 +4,55 @@ import "../styles/gameboard.css";
 import Tile from "./tile";
 
 class Gameboard extends Component {
+    // initialization with state creation
     constructor () {
         super();
 
         this.state = {
-            test: "testVal"
+            currentScore: 0,
+            clickedIDs: [],
         };
     }
 
-    render () {
-        return (
-            <div className={"container"}>
-                Gameboard Class Container
-                <h1>{this.state.test}</h1>
+    // create array of cards to be displayed
+    buildCards () {
+        let cards = [];
 
-                <Tile />
-            </div>
-        );
+        for (let i = 0; i < 12; i++) {
+            const thisCard = <Tile id={i} image={`some image ${i}`} num={i} cb={this.checkCard.bind(this)}/>;
+            cards.push(thisCard);
+        }
+
+        return cards;
+    }
+
+    // callback to check if the card has been clicked
+    checkCard (pID) {
+        console.log(`checking Card ${pID}`);
+        console.log(this.state.currentScore);
+        console.log(this.state.clickedIDs);
+
+        if (this.state.clickedIDs.includes(pID)) {
+            // lose the game
+            console.log('you lose');
+        } else {
+            // increase score and add to the clicked IDs
+            // cards should get reshuffled by buildCards on new render() when setState is called
+
+            this.state.clickedIDs.push(pID);
+            this.setState({ currentScore: this.state.currentScore + 1 });
+        }
+    };
+
+    // render the cards and any game info
+    render () {
+        const testText = <div className={"container"}>
+                            <h1 className={"headliner"}>Current Score: {this.state.currentScore}</h1>
+                            <h1 className={"headliner"}>Clicked Cards: {this.state.clickedIDs.join(` `)}</h1>
+                        </div>;
+        const newCards = this.buildCards();
+
+        return [testText, newCards];
     }
 }
 
--- client/src/components/tile.js
+++ client/src/components/tile.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/tile.css";
 
-const Tile = props => (
-    <div className={"tile"}>
-        <span>THIS IS A TILE</span>
-    </div>
-);
+class Tile extends Component {
+    constructor (props) {
+        super(props);
+
+        this.id = props.id;
+
+        this.state = {
+            image: props.image,
+            cardNum: props.num,
+        }
+    }
+
+    // onClick event function to be attached to each card
+    clickedMe = () => {
+        console.log(`You clicked ${this.id}`);
+        console.log(this);
+
+        this.props.cb(this.id);
+    };
+
+    render () {
+        return (
+            <div className={"tile"} onClick={this.clickedMe}>
+                <span>THIS IS A TILE</span>
+                <h3>{this.state.image}</h3>
+                <h4>{this.state.cardNum}</h4>
+            </div>
+        );
+    }
+}
 
 export default Tile;
